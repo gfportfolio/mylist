da9c24f7a0501c0e5501d4195a0c1b7cae9b0615
Gavin Farnsworth <gavin-farnsworth@leavitt.com>
2017-11-21 09:14:47 -0700

    Work on signout dialog

---
+++ securityconfig.js
@@ -0,0 +1,8 @@
+var config = {
+          apiKey: "AIzaSyBAo3nI8P5PrS-qD3LgJ-jqJMxta908vug",
+          authDomain: "mylists-356ab.firebaseapp.com",
+          databaseURL: "https://mylists-356ab.firebaseio.com",
+          storageBucket: "mylists-356abappspot.com",
+          messagingSenderId: "961388168525",
+          projectId: "mylists-356ab"
+        };
