066a0c4ced4006ef6b51abdd377c7fc7d7d8525c
Gavin Farnsworth <gavin-farnsworth@leavitt.com>
2018-02-22 09:07:41 -0700

    Got google api to send me a list of contacts

--- src/my-friend-add.js
+++ src/my-friend-add.js
@@ -5,42 +5,46 @@ class MyFriendAdd extends Polymer.Element {
 
   static get properties() {
     return {
-      userId: {type: Number},
-      route: {type: Object},
-      routeData: {type: Object, observer: '_routeChanged'},
-      isActive: {type: Boolean},
-      listId: {type: String},
+      userId: {
+        type: Number
+      },
+      route: {
+        type: Object
+      },
+      routeData: {
+        type: Object,
+        observer: '_routeChanged'
+      },
+      isActive: {
+        type: Boolean
+      },
+      listId: {
+        type: String
+      },
+      contacts: {
+        type: [Object]
+      }
     };
   }
 
   submitClick() {
     this.handleClientLoad();
-    // this._googleApiStart();
-    // if (this.listId !== undefined) {
-    //   firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid).collection('lists').doc(this.listId).collection('items').add({
-    //     name: this.$.nameInput.value,
-    //     url: this.$.urlInput.value,
-    //     photoUrl: this.$.photoUrlInput.value,
-    //     purchased: this.$.purchasedInput.checked
-    //   });
-    //   this.set('route.path', `lists-items/${this.listId}`);
-    // }
   }
   handleClientLoad() {
     let self = this;
     gapi.load('client', {
-      callback: function() {
+      callback: function () {
         // Handle gapi.client initialization.
         self.initClient();
         self._googleApiStart();
 
       },
-      onerror: function() {
+      onerror: function () {
         // Handle loading error.
         alert('gapi.client failed to load!');
       },
-      timeout: 5000,  // 5 seconds.
-      ontimeout: function() {
+      timeout: 5000, // 5 seconds.
+      ontimeout: function () {
         // Handle timeout.
         alert('gapi.client could not load in a timely manner!');
       }
@@ -57,53 +61,42 @@ class MyFriendAdd extends Polymer.Element {
   initClient() {
     // Initialize the client with API key and People API, and initialize OAuth with an
     // OAuth 2.0 client ID and scopes (space delimited string) to request access.
-    gapi.client.init({apiKey: config.apiKey, discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/people/v1/rest'], clientId: `${clientId}.apps.googleusercontent.com`, scope: 'https://www.googleapis.com/auth/contacts.readonly'}).then(function() {
-      // gapi.client.setToken();
+    let self = this;
 
+    gapi.client.init({
+      apiKey: config.apiKey,
+      discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/people/v1/rest'],
+      clientId: `${clientId}.apps.googleusercontent.com`,
+      scope: 'https://www.googleapis.com/auth/contacts.readonly'
+    }).then(function () {
+      // gapi.client.setToken();
+      self._googleApiStart();
     });
-  }  // https://developers.google.com/people/v1/read-people
+  } // https://developers.google.com/people/v1/read-people
   // https://developers.google.com/api-client-library/javascript/start/start-js
   _googleApiStart() {
+    let self = this;
+    this.contacts = new Array();
     gapi.client.people.people.connections
-        .list({
-          'resourceName': 'people/me',
-          'pageSize': 10,
-          'personFields': 'names,emailAddresses',
-        })
-        .then(function(response) {
-          var connections = response.result.connections;
-          appendPre('Connections:');
+      .list({
+        'resourceName': 'people/me',
+        'pageSize': 10,
+        'personFields': 'names,emailAddresses,Photos', //https://developers.google.com/people/api/rest/v1/people
+      })
+      .then(function (response) {
+        var connections = response.result.connections;
 
-          if (connections.length > 0) {
-            for (i = 0; i < connections.length; i++) {
-              var person = connections[i];
-              if (person.names && person.names.length > 0) {
-                appendPre(person.names[0].displayName)
-              } else {
-                appendPre('No display name found for connection.');
-              }
-            }
-          } else {
-            appendPre('No upcoming events found.');
-          }
-        });
-    // gapi.client.init({
-    //     'apiKey': config.apiKey,
-    //     'discoveryDocs': ['https://people.googleapis.com/$discovery/rest'],
-    //     'clientId': `${clientId}.apps.googleusercontent.com`,
-    //     'scope': 'profile',
-    //   }).then(function () {
-    //     return gapi.client.request({
-    //       'path': 'https://people.googleapis.com/v1/people/me?requestMask.includeField=person.names',
-    //     })
-    //   })
-    //   .then(
-    //     function (response) {
-    //       console.log(response.result);
-    //     },
-    //     function (reason) {
-    //       console.log('Error: ' + reason.result.error.message);
-    //     });
+        if (connections.length > 0) {
+          connections.forEach(element => {
+            self.contacts.push({
+              name: element.names[0].displayName,
+              emailAddress: element.emailAddresses[0].value,
+              photo: element.photos[0].url
+            })
+          });
+          console.log(self.contacts);
+        }
+      });
   }
 }
 window.customElements.define(MyFriendAdd.is, MyFriendAdd);
