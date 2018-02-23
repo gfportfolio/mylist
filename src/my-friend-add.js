61596f33a91f943a347892dc25246a29aaccc482
Gavin Farnsworth <gavin-farnsworth@leavitt.com>
2018-02-23 09:10:39 -0700

    Started contacts to friends page

--- src/my-friend-add.js
+++ src/my-friend-add.js
@@ -4,27 +4,7 @@ class MyFriendAdd extends Polymer.Element {
   }
 
   static get properties() {
-    return {
-      userId: {
-        type: Number
-      },
-      route: {
-        type: Object
-      },
-      routeData: {
-        type: Object,
-        observer: '_routeChanged'
-      },
-      isActive: {
-        type: Boolean
-      },
-      listId: {
-        type: String
-      },
-      contacts: {
-        type: [Object]
-      }
-    };
+    return {userId: {type: Number}, route: {type: Object}, routeData: {type: Object, observer: '_routeChanged'}, isActive: {type: Boolean}, listId: {type: String}, contacts: {type: [Object]}};
   }
 
   submitClick() {
@@ -33,18 +13,18 @@ class MyFriendAdd extends Polymer.Element {
   handleClientLoad() {
     let self = this;
     gapi.load('client', {
-      callback: function () {
+      callback: function() {
         // Handle gapi.client initialization.
         self.initClient();
         self._googleApiStart();
 
       },
-      onerror: function () {
+      onerror: function() {
         // Handle loading error.
         alert('gapi.client failed to load!');
       },
-      timeout: 5000, // 5 seconds.
-      ontimeout: function () {
+      timeout: 5000,  // 5 seconds.
+      ontimeout: function() {
         // Handle timeout.
         alert('gapi.client could not load in a timely manner!');
       }
@@ -63,40 +43,30 @@ class MyFriendAdd extends Polymer.Element {
     // OAuth 2.0 client ID and scopes (space delimited string) to request access.
     let self = this;
 
-    gapi.client.init({
-      apiKey: config.apiKey,
-      discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/people/v1/rest'],
-      clientId: `${clientId}.apps.googleusercontent.com`,
-      scope: 'https://www.googleapis.com/auth/contacts.readonly'
-    }).then(function () {
+    gapi.client.init({apiKey: config.apiKey, discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/people/v1/rest'], clientId: `${clientId}.apps.googleusercontent.com`, scope: 'https://www.googleapis.com/auth/contacts.readonly'}).then(function() {
       // gapi.client.setToken();
       self._googleApiStart();
     });
-  } // https://developers.google.com/people/v1/read-people
+  }  // https://developers.google.com/people/v1/read-people
   // https://developers.google.com/api-client-library/javascript/start/start-js
   _googleApiStart() {
     let self = this;
     this.contacts = new Array();
     gapi.client.people.people.connections
-      .list({
-        'resourceName': 'people/me',
-        'pageSize': 10,
-        'personFields': 'names,emailAddresses,Photos', //https://developers.google.com/people/api/rest/v1/people
-      })
-      .then(function (response) {
-        var connections = response.result.connections;
-
-        if (connections.length > 0) {
-          connections.forEach(element => {
-            self.contacts.push({
-              name: element.names[0].displayName,
-              emailAddress: element.emailAddresses[0].value,
-              photo: element.photos[0].url
-            })
-          });
-          console.log(self.contacts);
-        }
-      });
+        .list({
+          'resourceName': 'people/me',
+          'pageSize': 10,
+          'personFields': 'names,emailAddresses,Photos',  // https://developers.google.com/people/api/rest/v1/people
+        })
+        .then(function(response) {
+          var connections = response.result.connections;
+          var userContacts = new Array();
+          if (connections.length > 0) {
+            connections.forEach(element => {userContacts.push({name: element.names[0].displayName, emailAddress: element.emailAddresses[0].value, photo: element.photos[0].url})});
+            self.set('contacts', userContacts);
+            console.log(self.contacts);
+          }
+        });
   }
 }
 window.customElements.define(MyFriendAdd.is, MyFriendAdd);
