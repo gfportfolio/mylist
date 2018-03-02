f3540ddcfe059ff4f9550e96727bcc0e20a837e9
Gavin Farnsworth <gavin-farnsworth@leavitt.com>
2018-03-02 09:22:58 -0700

    Added firebase calls to save reviews, viewers, and friends from add friends page

--- src/my-friend-add.js
+++ src/my-friend-add.js
@@ -4,28 +4,7 @@ class MyFriendAdd extends Polymer.Element {
   }
 
   static get properties() {
-    return {
-      userId: {
-        type: Number
-      },
-      route: {
-        type: Object
-      },
-      routeData: {
-        type: Object,
-        observer: '_routeChanged'
-      },
-      isActive: {
-        type: Boolean,
-        observer: '_activeChanged'
-      },
-      listId: {
-        type: String
-      },
-      contacts: {
-        type: [Object]
-      }
-    };
+    return {userId: {type: Number}, route: {type: Object}, routeData: {type: Object, observer: '_routeChanged'}, isActive: {type: Boolean, observer: '_activeChanged'}, listId: {type: String}, contacts: {type: [Object]}};
   }
 
   submitClick() {
@@ -36,15 +15,15 @@ class MyFriendAdd extends Polymer.Element {
     if (!gapi.load)
       return;
     gapi.load('client', {
-      callback: function () {
+      callback: function() {
         self.initClient();
 
       },
-      onerror: function () {
+      onerror: function() {
         alert('gapi.client failed to load!');
       },
-      timeout: 5000, // 5 seconds.
-      ontimeout: function () {
+      timeout: 5000,  // 5 seconds.
+      ontimeout: function() {
         alert('gapi.client could not load in a timely manner!');
       }
     });
@@ -68,46 +47,46 @@ class MyFriendAdd extends Polymer.Element {
     // OAuth 2.0 client ID and scopes (space delimited string) to request access.
     let self = this;
 
-    gapi.client.init({
-      apiKey: config.apiKey,
-      discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/people/v1/rest'],
-      clientId: `${clientId}.apps.googleusercontent.com`,
-      scope: 'https://www.googleapis.com/auth/contacts.readonly'
-    }).then(function () {
+    gapi.client.init({apiKey: config.apiKey, discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/people/v1/rest'], clientId: `${clientId}.apps.googleusercontent.com`, scope: 'https://www.googleapis.com/auth/contacts.readonly'}).then(function() {
       // gapi.client.setToken();
       self._googleApiStart();
     });
-  } // https://developers.google.com/people/v1/read-people
+  }  // https://developers.google.com/people/v1/read-people
   // https://developers.google.com/api-client-library/javascript/start/start-js
 
   _googleApiStart() {
     let self = this;
     this.contacts = new Array();
     gapi.client.people.people.connections
-      .list({
-        'resourceName': 'people/me',
-        'pageSize': 10,
-        'personFields': 'names,emailAddresses,Photos', // https://developers.google.com/people/api/rest/v1/people
-      })
-      .then(function (response) {
-        var connections = response.result.connections;
-        var userContacts = new Array();
-        if (connections.length > 0) {
-          connections.forEach(element => {
-            userContacts.push({
-              name: element.names[0].displayName,
-              emailAddress: element.emailAddresses[0].value,
-              photo: element.photos[0].url
-            })
-          });
-          self.set('contacts', userContacts);
-          console.log(self.contacts);
-        }
-      });
+        .list({
+          'resourceName': 'people/me',
+          'pageSize': 10,
+          'personFields': 'names,emailAddresses,Photos',  // https://developers.google.com/people/api/rest/v1/people
+        })
+        .then(function(response) {
+          var connections = response.result.connections;
+          var userContacts = new Array();
+          if (connections.length > 0) {
+            connections.forEach(element => {userContacts.push({name: element.names[0].displayName, emailAddress: element.emailAddresses[0].value, photo: element.photos[0].url})});
+            self.set('contacts', userContacts);
+            console.log(self.contacts);
+          }
+        });
   }
 
-  _addToFriends(e) {
-    console.log(e.currentTarget.dataArgs);
+  _allowToSeeMyLists(e) {
+    let user = e.currentTarget.dataArgs;
+    let userData = {name: user.name, emailAddress: user.emailAddress, photoUrl: user.photo};
+    let firebaseUser = firebase.auth().currentUser;
+    let firebaseData = {name: firebaseUser.displayName, emailAddress: firebaseUser.email, photoUrl: firebaseUser.photoURL};
+    firebase.firestore().collection('users').doc(firebaseUser.email).collection('viewers').add(userData);
+    firebase.firestore().collection('users').doc(user.emailAddress).collection('friends').add(firebaseData);
+  }
+  _requestToSeeThereLists(e) {
+    let user = e.currentTarget.dataArgs;
+    let firebaseUser = firebase.auth().currentUser;
+    let firebaseData = {name: firebaseUser.displayName, emailAddress: firebaseUser.email, photoUrl: firebaseUser.photoURL};
+    firebase.firestore().collection('users').doc(user.emailAddress).collection('requests').add(firebaseData);
   }
 }
 window.customElements.define(MyFriendAdd.is, MyFriendAdd);
